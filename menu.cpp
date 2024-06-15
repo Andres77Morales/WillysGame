27b1d23f40f485c73ac7c6f3cb3cd39596aa74e1
Andres77Morales <162521065+Andres77Morales@users.noreply.github.com>
2024-06-15 15:48:18 -0400

    sef

---
+++ menu.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+void menu_();
+int main()
+{
+
+}
+
+void menu_()
+{
+    cout <<"----------------------------------------------";
+    cout <<"        BIENVENIDO AL WILLY GAME'S            ";
+    cout <<"----------------------------------------------";
+    cout <<" tiene cuenta o es nuevo por aca";
+    cout <<" (1) para abrir una nueva cuenta";
+    cout <<" (2) si tiene cuenta existente";
+}
+// con con para agragar
+void menu_1_agregarPersonas()
+{
+    cout<<" nombre :";
+    cout<<"apellido:";
+    cout<<"edad    :";
+    cout<<"carnet  :";
+    cout<<"contraseña :";
+    cout<< " proseso completado usted es en la lista";
+}
+void cuenta_existente()
+{
+    cout<<"ingrese su nombre :";
+    cout<<"ingrese su contraseña:";
+}
+void cuenta_del_propietario()
+{
+    cout<<"1 reporte de ventas mes";
+    cout<<"2 reporte de ventas año";
+    cout<<"3 reporte de cuentas ";
+    cout<<"4 facturacion de personas ";
+    cout<<"5 inventario de juegos";
+    cout<<"6 inventarios de consolas y mandos";
+}
+void menu_de_consolas()
+{
+    cout<<"consolas y mandos :";
+    cout<<"catalogo de juegos :";
+    /****************************/
+    cout<<"consolas--";
+    cout<<"1) nintendo ";
+    //-------
+    cout<<"1)consolas nintendo switch";
+    cout<<"2)mando simple que se puede divir";
+    cout<<"3)mando complex ";
+    //------
+    cout <<"2)xbox";
+    cout<<"3)xbox 6 genereacion";
+    /*   */
+    cout<<"play station";
+
+}
+void juegos()
+{
+    cout<<" terror"; 
+    /********/
+    cout<<"accion";
+    /*******/
+    cout<<"peleas";
+    /*******/
+    cout<<"musica";
+    /*******/
+}
