fb862612a75d4fa30fa3bf38545fca91cdd8e21a
Jaime Huaycho <jaimehuaycho@MacBook-Pro-de-Jaime.local>
2024-06-15 15:59:11 -0400

    primer cambio

---
+++ MENUS.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void menu_(){
+    cout <<"----------------------------------------------"<<endl;
+    cout <<"        BIENVENIDO AL WILLY GAME'S            "<<endl;
+    cout <<"----------------------------------------------"<<endl;
+    cout <<" tiene cuenta o es nuevo por aca"<<endl;
+    cout <<" (1) para abrir una nueva cuenta"<<endl;
+    cout <<" (2) si tiene cuenta existente"<<endl;
+}
